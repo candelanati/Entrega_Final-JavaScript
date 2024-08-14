4df1b7c1aafd98a86e3f5a70c07f06b5109f9a73
candelanati <candelanati23@gmail.com>
2024-08-14 20:30:02 -0300

    basicos carrito

--- main.js
+++ main.js
@@ -7,31 +7,76 @@ const Productos = [
     {
         titulo: "Across the spiderverse",
         imagen: "./img/across_the_spiderverse.jpg",
-        precio: "9000"
+        precio: 9000
     },
     {
         titulo:"Happier than ever",
         imagen:"./img/happier_than_ever.jpg",
-        precio: "9500"
+        precio: 9500
     },
     {
         titulo:"Hit me hard and soft",
         imagen:"./img/hit_me_hard_and_soft.jpg",
-        precio: "9500"
+        precio: 11000
     },
     {
         titulo:"Currents",
         imagen:"./img/Currents.jpg",
-        precio: "9500"
+        precio: 10500
     }
 ]
+
+const creaCarrito = (titulo, precio) => {
+    const tarjeta = document.createElement("div")
+    const tituloDOM = document.createElement("h3")
+    const precioDOM = document.createElement("p")
+
+    tarjeta.classList.add("tarjeta-carrito")
+    tituloDOM.classList.add("titulo")
+    precioDOM.classList.add("precio")
+
+    tituloDOM.innerText = titulo
+    precioDOM.innerText = "$" + precio
+
+    tarjeta.appendChild(tituloDOM)
+    tarjeta.appendChild(precioDOM)
+    
+    return tarjeta
+}
+
+const actualizaCarrito =() =>{
+    carrito.innerHTML = ""
+    Carrito.forEach(element =>{
+        carrito.appendChild(creaCarrito(element.titulo,element.precio))
+    })
+}
+
+const agregaCarrito = (titulo, precio) => {
+    const bool = Carrito.some(element =>{
+       return element.titulo === titulo
+    })
+    if(bool){
+        const producto = Carrito.find (element =>{
+            return element.titulo === titulo
+        })
+        producto.cantidad += 1
+    }else{
+        Carrito.push({
+            titulo,
+            precio,
+            cantidad: 1
+        })
+    }
+    actualizaCarrito()
+}
 
 const creaCards = (imagen, titulo, precio) => {
     const tarjeta = document.createElement("div")
     const imagenDOM = document.createElement("img")
     const tituloDOM = document.createElement("h3")
     const precioDOM = document.createElement("p")
-    
+    const botonDOM = document.createElement("button")
+
     tarjeta.classList.add("tarjeta")
     imagenDOM.classList.add("imagen")
     tituloDOM.classList.add("titulo")
@@ -39,17 +84,26 @@ const creaCards = (imagen, titulo, precio) => {
 
     tituloDOM.innerText = titulo
     precioDOM.innerText = "$" + precio
+    botonDOM.innerText = "comprar"
     imagenDOM.src = imagen
-    
+
+    botonDOM.addEventListener("click", () =>{
+        agregaCarrito(titulo, precio)
+    })
+
     tarjeta.appendChild(imagenDOM)
     tarjeta.appendChild(tituloDOM)
     tarjeta.appendChild(precioDOM)
+    tarjeta.appendChild(botonDOM)
     
     return tarjeta
 }
+
+
 
 Productos.forEach(element => {
     const productoDom = creaCards(element.imagen, element.titulo,element.precio)
 
     productos.appendChild(productoDom)
 });
+
